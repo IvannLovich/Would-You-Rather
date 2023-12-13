95bf20f17739e4cdc61e6da0dc0a31addf671a94
Iván Lovich <ivan.n.lovich@gmail.com>
2023-12-13 15:27:10 -0300

    fixes on redux developer tools extension

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { applyMiddleware, createStore, compose } from 'redux';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { applyMiddleware, createStore, compose } from "redux";
+import { Provider } from "react-redux";
 
-import thunk from 'redux-thunk';
-import mainReducer from './reducers';
-import './index.css';
-import App from './App';
+import thunk from "redux-thunk";
+import mainReducer from "./reducers";
+import "./index.css";
+import App from "./App";
 
 const store = createStore(
   mainReducer,
   compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__(),
-  ),
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : (args) => args
+  )
 );
 
 ReactDOM.render(
@@ -23,5 +24,5 @@ ReactDOM.render(
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
